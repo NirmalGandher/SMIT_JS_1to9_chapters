8f2fd77b2d8d80b39c60f3ebb0bbdbed93cb925a
NIRMAL <nirmalnirmalkhipro@gmail.com>
2024-09-14 12:58:30 +0500

    assignment5to9

---
+++ app.js
@@ -0,0 +1,59 @@
+   // // Initial value
+   // var a = 10;
+        
+   // // Display the initial value
+   // document.write("The value a is: " + a + "<br>");
+
+   // document.write('....................................'+"<br>")
+   
+   // // Increment the value
+   // ++a;
+   
+   // // Display the incremented value
+   // document.write(" Now The value a is: " + a+ "<br>");
+
+   
+
+   //  // Display the initial value
+   //  document.write("The value a is: " + a + "<br>");
+   
+   //  // Increment the value
+   //  a++;
+    
+   //  // Display the incremented value
+   //  document.write(" Now The value a is: " + a+ "<br>");
+
+
+   //  // Display the initial value
+   //  document.write("The value a is: " + a + "<br>");
+   
+   //  // Increment the value
+   //  --a;
+    
+   //  // Display the incremented value
+   //  document.write(" Now The value a is: " + a+ "<br>");
+
+   //  document.write("The value a is: " + a + "<br>");
+   //  a++
+   //  document.write(" Now The value a is: " + a + "<br>");
+
+   var a=2;
+   var b=1;
+   var result = --a - --b + ++b +b--;
+   document.write(result);
+
+
+   //--a=1 because here --a means first 1 minus then value will be calculate
+   //--b=0 because here --b means first 1 minus then value will be calculate
+   //[(--a)-(--b)]=(1)-(0)=1 
+   //++b=2 because here ++b means first 1 add in b then value will be calculate
+   //[(--a)-(--b)]+[++b]=[1]+[2]=3
+   //b--=1 first value calculate and then will subtract 
+   //[(--a)-(--b)]+[++b]+[b--]=[1]+[2]+[1]=[1]+[2]+[0]=3
+
+
+
+
+ 
+
+   
